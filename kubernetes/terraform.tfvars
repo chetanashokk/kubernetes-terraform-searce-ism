ec2a0b286dc459752c3f83b9376789104834decc
Chetan Khobragade <chetankhobragade@C02FN483Q6LR.local>
2023-05-12 08:55:34 +0530

    Added real one

---
+++ kubernetes/terraform.tfvars
@@ -0,0 +1,12 @@
+project_id= "searce-msp-gcp"
+
+name = "chetan-test-ism"
+
+regional = false
+
+zones = ["us-central1-a"]
+
+network = "chetantest"
+
+subnetwork = "chetantest"
+
--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// This file was automatically generated from a template in ./autogen/main
-
 /******************************************
   Get available zones in region
  *****************************************/
@@ -92,7 +74,6 @@ locals {
     security_group = var.authenticator_security_group
   }]
 
-  // legacy mappings https://github.com/hashicorp/terraform-provider-google/pull/10238
   old_node_metadata_config_mapping = { GKE_METADATA_SERVER = "GKE_METADATA", EXPOSE = "GCE_METADATA" }
 
   cluster_node_metadata_config = var.node_metadata == "UNSPECIFIED" ? [] : [{
